c4a51eb21ced8f56fd7b541458e58d834fea8a4b
NadezhdaSvetlakova <nadezhda.svetlakova@accenture.com>
2021-01-22 23:16:23 +0200

    add header with burger and menu

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 "use strict";
+document.getElementById('header_burger').onclick = function() {
+    document.getElementById('header_burger').classList.toggle('active');
+    document.getElementById('header_menu').classList.toggle('active');
+}
