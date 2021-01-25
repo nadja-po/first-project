6eced0ffe5d1a851e16d4338e41a9777ae7e4361
NadezhdaSvetlakova <nadezhda.svetlakova@accenture.com>
2021-01-25 22:22:35 +0200

    add cart in header

--- script.js
+++ script.js
@@ -2,4 +2,5 @@
 document.getElementById('header_burger').onclick = function() {
     document.getElementById('header_burger').classList.toggle('active');
     document.getElementById('header_menu').classList.toggle('active');
+    document.getElementById('body').classList.toggle('lock');
 }
